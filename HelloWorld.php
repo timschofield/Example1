f9114163292ce158278b8131a4d31876be06e047
Tim Schofield <tim@weberpafrica.com>
2013-06-05 12:53:47 +0100

    Initial upload

---
+++ HelloWorld.php
@@ -0,0 +1,49 @@
+<?php
+
+include('includes/session.inc');
+
+$Title = _('Hello World');
+
+include('includes/header.inc');
+
+echo '<p class="page_title_text noPrint" ><img src="'.$RootPath.'/css/'.$Theme.'/images/user.png" width="24px" title="' . _('Hello World') . '" alt="" />' . _('Hello World') . '</p>';
+
+if (isset($_POST['Submit'])) {
+	/* Get clients IP address */
+	$IPAddress = $_SERVER['REMOTE_ADDR'];
+
+	/* Has user been here before? */
+	$SQL = "SELECT count(name) as names
+				FROM helloworld
+				WHERE name='" . $_POST['Name'] . "'
+					AND ipaddress='" . $IPAddress . "'";
+	$Result = DB_query($SQL, $db);
+	$MyRow = DB_fetch_array($Result);
+	if ($MyRow['names'] > 0) {
+		prnMsg( _('Welcome back') . ' ' . $_POST['Name'], 'info');
+	} else {
+		$SQL = "INSERT INTO helloworld (ipaddress,
+										name)
+									VALUES (
+										'" . $IPAddress . "',
+										'" . $_POST['Name'] . "'
+									)";
+		$Result = DB_query($SQL, $db);
+		prnMsg( _('Hello') . ' ' . $_POST['Name'], 'info');
+	}
+} else {
+	echo '<div class="page_help_text noPrint">' . _('Enter your name in the box below.') . '</div>';
+	echo '<form enctype="multipart/form-data" action="' . htmlspecialchars($_SERVER['PHP_SELF'],ENT_QUOTES,'UTF-8') . '" method="post" class="noPrint">';
+	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+	echo '<div class="centre">
+			<input type="text" name="Name" />
+		</div>
+		<div class="centre">
+			<input type="submit" name="Submit" value="Submit" />
+		</div>';
+	echo '</form>';
+}
+
+include('includes/footer.inc');
+
+?>
